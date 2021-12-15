6bca1588ae63fac9875f5c03d356038a830b8c28
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-12-15 22:40:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
+    spaceBetween: 20,
     effect: "fade",
     autoplay:true,
+    loop : true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
