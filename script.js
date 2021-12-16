9d04551e463162bf734c8b24c09670d52d27e7cd
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-12-16 15:37:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ let preload = document.getElementById('loading');
 function myfun(){
   preload.style.display="none";
 }
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  autoplay:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
